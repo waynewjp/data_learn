5cfb247256cf61fe6d3f5a886335807cc5c80e8b
waynewjp <wanjingping@aliyun.com>
2021-03-05 15:00:01 +0800

    new add

--- .virtual_documents/PIL图像库使用.ipynb.py
+++ .virtual_documents/PIL图像库使用.ipynb.py
@@ -12,11 +12,13 @@ im=Image.fromarray(b.astype('uint8'))
 im.save('/home/www/jupyter_lab/素材/图片2.jpg')
 
 
-a=np.array(Image.open('/home/www/jupyter_lab/素材/图片1.jpg').convert('L')
+from PIL import Image
+import numpy as np
+a=np.array(Image.open('/home/www/jupyter_lab/素材/图片1.jpg').convert('L'))
 # convert('L')将图片的灰度值提取处理，三位RGB数组变成灰度值的二维数组
+print(a.shape,a.dtype)
+
+
 b=255-a
 im=Image.fromarray(b.astype('uint8'))
-b.save('/home/www/jupyter_lab/素材/图片3.jpg')
-
-
-
+im.save('/home/www/jupyter_lab/素材/图片3.jpg')
