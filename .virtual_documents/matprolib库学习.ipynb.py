82845a8df45b000bb267c2b0d6090c38a6ce853b
waynewjp <wanjingping@aliyun.com>
2021-03-05 15:50:01 +0800

    new add

--- .virtual_documents/matprolib库学习.ipynb.py
+++ .virtual_documents/matprolib库学习.ipynb.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 
 
 plt.plot([3,1,4,5,2])
+# 输入的一维数组是Y轴，X轴是元素的索引，【0，1，2，3，4】
 plt.ylabel('grade')
 plt.show()
+plt.savefig('/home/www/jupyter_lab/素材/test',dpi=600) #默认是png格式
 
 
 
