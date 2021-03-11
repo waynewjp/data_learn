435c1059b1984d5091138782638e18048704ddcc
waynewjp <wanjingping@aliyun.com>
2021-03-11 15:00:01 +0800

    new add

--- .virtual_documents/matprolib库学习.ipynb.py
+++ .virtual_documents/matprolib库学习.ipynb.py
@@ -9,4 +9,12 @@ plt.savefig('/home/www/jupyter_lab/素材/test',dpi=600) #默认是png格式
 
 
 
+#两个参数，一个x，一个y
+plt.plot([0,2,4,6,8],[3,1,4,5,2])
+plt.ylabel('成绩')
+#axis设定横纵坐标尺度：-1,10表示x轴坐标从-1到10，0,6表示y轴从0-6
+plt.axis([-1,10,0,6])
+plt.show()
+
+
 
