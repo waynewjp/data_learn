a446fdd34584204f2fe69333f809d694c7a6d2df
waynewjp <wanjingping@aliyun.com>
2021-03-11 16:00:01 +0800

    new add

--- .virtual_documents/matprolib库学习.ipynb.py
+++ .virtual_documents/matprolib库学习.ipynb.py
@@ -9,17 +9,20 @@ plt.savefig('/home/www/jupyter_lab/素材/test',dpi=600) #默认是png格式
 
 
 
-import matplotlib
 import matplotlib.pyplot as plt
-#设置字体为楷体
-matplotlib.rcParams['font.sans-serif']=['SimHei'] #显示中文标签
-matplotlib.rcParams['axes.unicode_minus']=False   #这两行需要手动设置
+import matplotlib
+matplotlib.rcParams['font.family']='SimHei'
+matplotlib.rcParams['font.size']=20
+
 #两个参数，一个x，一个y
 plt.plot([0,2,4,6,8],[3,1,4,5,2])
-plt.ylabel('成绩')
+plt.ylabel('成绩',fontpro)
 #axis设定横纵坐标尺度：-1,10表示x轴坐标从-1到10，0,6表示y轴从0-6
 plt.axis([-1,10,0,6])
 plt.show()
+
+
+
 
 
 import numpy as np
