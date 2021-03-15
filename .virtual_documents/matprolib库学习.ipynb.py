2285f3780d8375123ae83b74b28a6caa86205236
waynewjp <wanjingping@aliyun.com>
2021-03-15 16:20:01 +0800

    new add

--- .virtual_documents/matprolib库学习.ipynb.py
+++ .virtual_documents/matprolib库学习.ipynb.py
@@ -56,11 +56,13 @@ plt.title(r'郑泫渤')
 plt.show()
 
 
+import matplotlib.pyplot as plt
+labels='frog','dog','log','hog'
+size=[15,30,45,10] #比例
+explode=(0,0.1,0,0)
 
-
-
-
-
+plt.pie(size,explode=explode,labels=labels,autopct='get_ipython().run_line_magic("1.1f%%',shadow=False,startangle=90)", "")
+plt.show
 
 
 
