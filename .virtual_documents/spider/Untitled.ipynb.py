762d5e9f919690e9a1a22b1969ee7085174d7f04
waynewjp <wanjingping@aliyun.com>
2021-07-19 09:30:01 +0800

    new add

--- .virtual_documents/spider/Untitled.ipynb.py
+++ .virtual_documents/spider/Untitled.ipynb.py
@@ -2,8 +2,16 @@ from  requests_html import HTMLSession,UserAgent
 
 
 session=HTMLSession()
-ua=UserAgent().random()
+ua=UserAgent().random
 url='https://movie.douban.com/tag/#/?sort=U&range=0,10get_ipython().run_line_magic("27%27&tags=%E7%94%B5%E5%BD%B1,2020'", "")
 
 
-r=session.get()
+r=session.get(url,headers={'User-Agent':ua})
+r.encoding='gb2312'  
+
+
+print(r.text)
+
+
+
+
