796d24dc04ab7b1f6d8bc0fd80c80549b23a7c77
waynewjp <wanjingping@aliyun.com>
2021-07-21 15:20:01 +0800

    new add

--- .virtual_documents/spider/Untitled.ipynb.py
+++ .virtual_documents/spider/Untitled.ipynb.py
@@ -1,1 +1,24 @@
-from request_html import HTMLSession
+from  requests_html import HTMLSession,UserAgent
+import os
+
+
+def get_header():
+    location = os.getcwd() + '/fake_useragent.json'
+    ua = UserAgent(path=location)
+    return ua.random
+
+
+session=HTMLSession()
+ua = get_header()
+url='http://news.youth.cn/jsxw/index.htm'
+
+
+r=session.get(url,headers={'User-Agent':ua})
+r.encoding='GB2312' 
+
+
+if r.status_code == 200:
+    print(r.html.html)
+
+
+
